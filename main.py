b58f3a1cbaad7372625dfb493ba07445121c3932
Jakub Młocek <72828359+JakubMlocek@users.noreply.github.com>
2025-11-09 19:35:37 +0100

    created evaluations file

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import os
-import pandas as pd
-import sys
-import json
-import re
-from dataclasses import dataclass
-from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from agents_logic import CodeAgents, AgentConfig
 from evaluations import evaluate_vulnerability_fix_dataset
-
-
-
-
 
 if __name__ == "__main__":
     evaluate_vulnerability_fix_dataset()
